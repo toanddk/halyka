99f6481f37c827cf1330fd0fa623a2c2bca1145f
toanddk <toanddk@gmail.com>
2026-08-14 15:41:20 +0900

    them data

--- data/vietnamese/reading/vietnameseReadingLevel1.js
+++ data/vietnamese/reading/vietnameseReadingLevel1.js
@@ -42,18 +42,19 @@ const vietnameseReadingLevel1 = [
             },
 
             {
-                icon: "👩‍🏫",
+                icon: "👧",
 
                 blend: "ch + ị →",
 
-                word: "chị",
+                word: "bố",
 
                 blendAudio:
                     "vietnamese/reading/ch-i.mp3",
-
+                
                 wordAudio:
                     "vietnamese/reading/chi.mp3"
             },
+
                {
                 icon: "👶",
 
@@ -122,6 +123,20 @@ const vietnameseReadingLevel1 = [
 
                 wordAudio:
                     "vietnamese/reading/ca.mp3"
+            },
+
+            {
+                icon: "🍦",
+
+                blend: "k + em →",
+
+                word: "kem",
+
+                blendAudio:
+                    "vietnamese/reading/k-em.mp3",
+                
+                    wordAudio:
+                    "vietnamese/reading/kem.mp3"
             }
 
         ]
@@ -237,8 +252,431 @@ const vietnameseReadingLevel1 = [
             }
 
         ]
-    }
+    },
 
+// =================================================
+    // BÀI 5
+    // =================================================
+
+    {
+        id: "reading_5",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "🐱",
+
+                blend: "m + èo →",
+
+                word: "mèo",
+
+                blendAudio:
+                    "vietnamese/reading/m-eo.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/meo.mp3"
+            },
+
+            {
+                icon: "🍌",
+
+                blend: "ch + uối →",
+
+                word: "chuối",
+
+                blendAudio:
+                    "vietnamese/reading/ch-uoi.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/chuoi.mp3"
+            },
+
+            {
+                icon: "🥛",
+
+                blend: "s + ữa →",
+
+                word: "sữa",
+
+                blendAudio:
+                    "vietnamese/reading/s-ua.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/sua.mp3"
+            },
+
+            {
+                icon: "🐔",
+
+                blend: "g + à →",
+
+                word: "gà",
+
+                blendAudio:
+                    "vietnamese/reading/g-a.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/ga.mp3"
+            }
+
+        ]
+    },
+
+
+    // =================================================
+    // BÀI 6
+    // =================================================
+
+    {
+        id: "reading_6",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "👀",
+
+                blend: "m + ắt →",
+
+                word: "mắt",
+
+                blendAudio:
+                    "vietnamese/reading/m-at.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/mat.mp3"
+            },
+
+            {
+                icon: "👂",
+
+                blend: "t + ai →",
+
+                word: "tai",
+
+                blendAudio:
+                    "vietnamese/reading/t-ai.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/tai.mp3"
+            },
+
+            {
+                icon: "👃",
+
+                blend: "m + ũi →",
+
+                word: "mũi",
+
+                blendAudio:
+                    "vietnamese/reading/m-ui.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/mui.mp3"
+            },
+
+            {
+                icon: "👄",
+
+                blend: "m + iệng →",
+
+                word: "miệng",
+
+                blendAudio:
+                    "vietnamese/reading/m-ieng.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/mieng.mp3"
+            }
+
+        ]
+    },
+
+
+    // =================================================
+    // BÀI 7
+    // =================================================
+
+    {
+        id: "reading_7",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "🍚",
+
+                blend: "ă + n →",
+
+                word: "ăn",
+
+                blendAudio:
+                    "vietnamese/reading/a-n.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/an.mp3"
+            },
+
+            {
+                icon: "🥤",
+
+                blend: "u + ống →",
+
+                word: "uống",
+
+                blendAudio:
+                    "vietnamese/reading/u-ong.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/uong.mp3"
+            },
+
+            {
+                icon: "🚶",
+
+                blend: "đ + i →",
+
+                word: "đi",
+
+                blendAudio:
+                    "vietnamese/reading/d-i.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/di.mp3"
+            },
+
+            {
+                icon: "🏃",
+
+                blend: "ch + ạy →",
+
+                word: "chạy",
+
+                blendAudio:
+                    "vietnamese/reading/ch-ay.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/chay.mp3"
+            }
+
+        ]
+    },
+
+
+    // =================================================
+    // BÀI 8
+    // =================================================
+
+    {
+        id: "reading_8",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "🏠",
+
+                blend: "nh + à →",
+
+                word: "nhà",
+
+                blendAudio:
+                    "vietnamese/reading/nh-a.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/nha.mp3"
+            },
+
+            {
+                icon: "🌳",
+
+                blend: "c + ây →",
+
+                word: "cây",
+
+                blendAudio:
+                    "vietnamese/reading/c-ay.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/cay.mp3"
+            },
+
+            {
+                icon: "🌸",
+
+                blend: "h + oa →",
+
+                word: "hoa",
+
+                blendAudio:
+                    "vietnamese/reading/h-oa.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/hoa.mp3"
+            },
+
+            {
+                icon: "☀️",
+
+                blend: "n + ắng →",
+
+                word: "nắng",
+
+                blendAudio:
+                    "vietnamese/reading/n-ang.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/nang.mp3"
+            }
+
+        ]
+    },
+
+
+    // =================================================
+    // BÀI 9
+    // =================================================
+
+    {
+        id: "reading_9",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "🐶",
+
+                blend: "ch + ó →",
+
+                word: "chó",
+
+                blendAudio:
+                    "vietnamese/reading/ch-o.mp3",
+
+                wordAudio:"vietnamese/reading/cho.mp3"
+            },
+
+            {
+                icon: "🐱",
+
+                blend: "m + èo →",
+
+                word: "mèo",
+
+                blendAudio:
+                    "vietnamese/reading/m-eo.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/meo.mp3"
+            },
+
+            {
+                icon: "🐔",
+
+                blend: "g + à →",
+
+                word: "gà",
+
+                blendAudio:
+                    "vietnamese/reading/g-a.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/ga.mp3"
+            },
+
+            {
+                icon: "🐟",
+
+                blend: "c + á →",
+
+                word: "cá",
+
+                blendAudio:
+                    "vietnamese/reading/c-a.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/ca.mp3"
+            }
+
+        ]
+    },
+
+
+    // =================================================
+    // BÀI 10
+    // =================================================
+
+    {
+        id: "reading_10",
+
+        name: "Ghép âm",
+
+        items: [
+
+            {
+                icon: "🍎",
+
+                blend: "qu + ả →",
+
+                word: "quả",
+
+                blendAudio:
+                    "vietnamese/reading/qu-a.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/qua.mp3"
+            },
+
+            {
+                icon: "🎒",
+
+                blend: "c + ái →",
+
+                word: "cái",
+
+                blendAudio:
+                    "vietnamese/reading/c-ai.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/cai.mp3"
+            },
+
+            {
+                icon: "👦",
+
+                blend: "b + ạn →",
+
+                word: "bạn",
+
+                blendAudio:
+                    "vietnamese/reading/b-an.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/ban.mp3"
+            },
+
+            {
+                icon: "🏫",
+
+                blend: "l + ớp →",
+
+                word: "lớp",
+
+                blendAudio:
+                    "vietnamese/reading/l-op.mp3",
+
+                wordAudio:
+                    "vietnamese/reading/lop.mp3"
+            }
+
+        ]
+    }
 
 
 ];
